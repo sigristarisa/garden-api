b92d0addce76a74af1fc8109145532a611f82f5a
sigristarisa <arisasigrist.ch@gmail.com>
2023-05-11 17:55:15 +0200

    setup database

--- garden/garden/settings.py
+++ garden/garden/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,8 +79,13 @@ WSGI_APPLICATION = 'garden.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'DB_HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('PORT')
+        
     }
 }
 
--- garden/garden_api/models.py
+++ garden/garden_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Plant(models.Model):
+    plant_id = models.IntegerField()
+    name = models.CharField(max_length=20)
+    type = models.CharField(max_length=10)
+    seedlings_per_square = models.IntegerField()
+    companions = ArrayField(models.CharField(max_length=80))
+    enemies = ArrayField(models.CharField(max_length=80))
+    created = models.DateTimeField(auto_now=False, auto_now_add=False)
+    edited = models.DateTimeField(auto_now=True, auto_now_add=False)
+    url = models.CharField(max_length=80)
